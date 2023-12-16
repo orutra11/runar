fead7e8a26fd8ca84da9f06357580dee1d56d03a
arturo <orutra11@gmail.com>
2023-12-16 16:30:12 +0100

    Basic jinja/sqlite integration

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, url_for
+import sqlite3
+import re
+from flask import Flask, render_template, abort
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 
@@ -10,7 +12,28 @@ def index():
 
 @app.route("/activity/<activity_id>")
 def activityPage(activity_id):
-    return render_template("activity_detail.html", activity_id=activity_id)
+    id_regex = "^[0-9]{10,11}$"
+    regex_result = re.match(id_regex, activity_id)
+    if not regex_result:
+        abort(404)
+
+    con = sqlite3.connect("/Users/arturo/HealthData/DBs/garmin_activities.db")
+    cur = con.cursor()
+    res = cur.execute(
+        f"""
+            SELECT activity_id, start_time, name, distance, moving_time, tag_str 
+            FROM SMRY_activity_list WHERE activity_id = {activity_id}
+        """
+    )
+    act = res.fetchone()
+
+    if act is None:
+        abort(404)
+
+    activity_details = {}
+    activity_details["name"] = act[2]
+
+    return render_template("activity_detail.html", activity_details=activity_details)
 
 
 if __name__ == "__main__":
