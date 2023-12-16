467160ecb3a3da8f0cc54fbbaacdc05aebd6b057
arturo <orutra11@gmail.com>
2023-12-16 15:42:31 +0100

    Basic usage

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__, static_folder="static", template_folder="templates")
 
@@ -8,9 +8,9 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/test_redirect")
-def admin():
-    return redirect(url_for("index"))
+@app.route("/activity/<activity_id>")
+def activityPage(activity_id):
+    return render_template("activity_detail.html", activity_id=activity_id)
 
 
 if __name__ == "__main__":
